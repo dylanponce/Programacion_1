6f1526a544aaae98ce88b52e051fc1b9a5ab411f
Dylan Ponce <dylan18ponce2323@gmail.com>
2025-04-10 21:51:34 -0300

    Upload-3-Objetos
    Se completo la clase Fecha

--- Fecha.java
+++ Fecha.java
@@ -1,9 +1,9 @@
 package objetos;
 
 public class Fecha {
-	int dia;
-	int mes;
-	int anio;
+	private int dia;//Intancia
+	private int mes;
+	private int anio;
 	
 	public Fecha() {
 		this.dia=1;
@@ -15,7 +15,6 @@ public class Fecha {
 		this.mes=mes;
 		this.anio=anio;
 	}
-	
 	public void imprimir(){//Metodo Imprimir
 		System.out.println(this.dia+"/"+this.mes+"/"+this.anio);
 	}
@@ -23,11 +22,58 @@ public class Fecha {
 	public String toString() {//Metodo toString
 		return ""+this.dia+"/"+this.mes+"/"+this.anio;//Para Syso
 	}
-	//Ejercicio 1
-	public static int diaDelMes(int mes,int anio) {
-		if(mes<1||mes>12) {
-			return 0;
+	
+	boolean esVerano() {
+		return (this.mes==1||this.mes==2)||
+				(this.mes==12 && this.dia>=21)||
+				(this.mes==3 && this.dia<=21);
+			
 		}
+	
+	//Getters and Setters
+	
+	//Getters
+	public int getDia() {
+		return this.dia;
+	}
+	public int getMes() {
+		return this.mes;
+	}
+	public int getAnio() {
+		return this.anio;
+	}
+	
+	//Setters
+	public void setDia(int dia) {
+		if(dia<1 || dia>diaDelMes(this.mes,this.anio)) {
+			System.out.println("Fecha Invalida");
+		}
+		else {
+			this.dia = dia;			
+		}
+	}
+	public void setMes(int mes) {
+		if(mes<1 || mes>12) {
+			System.out.println("Fecha Invalida");
+		}
+		else {
+			this.mes = mes;			
+		}
+	}
+	public void setAnio(int anio) {
+		if(anio<1) {
+			System.out.println("Fecha Invalida");
+		}
+		this.anio = anio;
+	}
+	
+	
+	
+	//Ejercicio 1
+	public static int diaDelMes(int mes,int anio) {//metodo de clase
+		//if(mes<1||mes>12) {
+		//	return 0;
+		//}
 		if(mes==2) {
 			if (esBiciesto(anio)) {
 				return 29;
@@ -39,10 +85,126 @@ public class Fecha {
 		}
 		return 31;
 	}
+	
 	public static boolean esBiciesto(int anio) {
 		return (anio % 4 == 0 && anio % 100 != 0) || (anio % 400 == 0);
 	}
 	
 	//Ejercicio 2
+	public boolean esValida() {
+		if(this.anio<1) {
+			return false;
+		}
+		if (this.mes<=1 || this.mes>=12){
+			return false;
+		} 
+		if (this.dia<1 || this.dia>diaDelMes(this.mes,this.anio)) {
+			return false;
+		}
+		return true;
+	}
 	
+	//Ejercicio 3
+	public void avanzarDia() {
+		if(this.dia==diaDelMes(this.mes,this.anio)) {
+			this.dia=1;			
+			if(this.mes==12) {
+				this.mes=1;
+				this.anio++;
+			}
+			else {
+				this.mes++;
+			}
+		}
+		else {
+			this.dia++;
+		}
+	}
+	
+	//Ejercicio 3 v2
+	public void avanzarDiav2() {
+		Fecha prueba1 = new Fecha(this.dia+1,this.mes,this.anio);
+		if(prueba1.esValida()) {
+			this.dia++;
+		}
+		else{
+			Fecha prueba2 = new Fecha(1,this.mes+1,this.anio);
+			if(prueba2.esValida()) {
+				this.dia=1;
+				this.mes++;
+			}
+			else {
+				Fecha prueba3 = new Fecha(1,1,this.anio+1);//No hace falta
+				if(prueba3.esValida()) {
+					this.dia=1;
+					this.mes=1;
+					this.anio++;
+				}
+					
+			}
+		}
+	}
+	
+	//Ejercicio 4
+	public boolean antesQue(Fecha otra) {
+		if(this.anio<otra.anio) {
+			return true;
+		}
+		else {
+			if((this.anio==otra.anio) && (this.mes<otra.mes)) {
+				return true;
+			}
+			else {
+				if
+				((this.anio==otra.anio)&& (this.mes==otra.mes) && (this.dia<otra.dia)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	//Ejercicio 5
+	public int diaDelAnio() {
+		int cont=this.dia;
+		for(int i =this.mes-1;i>0;i--) {
+			cont=cont+(diaDelMes(i, this.anio));
+		}
+		return cont;
+	}
+	
+	//Ejercicio 6
+	public int diasDeDiferenciaCon(Fecha otra) {
+	    Fecha menor, mayor;
+
+	    if (this.antesQue(otra)) {
+	        menor = this;
+	        mayor = otra;
+	    } else {
+	        menor = otra;
+	        mayor = this;
+	    }
+
+	    if (menor.anio == mayor.anio) {//Si es el mismo año
+	        return Math.abs(menor.diaDelAnio() - mayor.diaDelAnio());
+	    }
+	    int diasEnMenorAnio;
+	    if (esBiciesto(menor.anio)) {//Días restantes del año inicial
+	        diasEnMenorAnio = 366;
+	    } else {
+	        diasEnMenorAnio = 365;
+	    }
+	    int cont = diasEnMenorAnio - menor.diaDelAnio();
+
+	    cont += mayor.diaDelAnio();	    //Días del año final
+	    
+	    for (int i = menor.anio + 1; i < mayor.anio; i++) {	    //Años intermedios
+	        if (esBiciesto(i)) {
+	            cont += 366;
+	        } else {
+	            cont += 365;
+	        }
+	    }
+	    return cont;
+	}
 }
--- Main.java
+++ Main.java
@@ -6,42 +6,55 @@ public class Main {
 	public static void main(String[] args) {
 		//OBJETOS
 		//Ejercicio 1
-			//Point p1=new Point(2,4);//Si no defino es (0,0)
-			//Point p2=new Point(3,5);
-			//System.out.println(Objetos.distancia(p1,p2));
+			Point p1=new Point(2,4);//Si no defino es (0,0)
+			Point p2=new Point(3,5);
+			System.out.println(Objetos.distancia(p1,p2));
 		//Ejercicio 2
-			//Rectangle n1=new Rectangle(0,0,3,4);
-			//System.out.println(Objetos.diagonal(n1));
+			Rectangle n1=new Rectangle(0,0,3,4);
+			System.out.println(Objetos.diagonal(n1));
 		//Ejercicio 3		
-			//Rectangle c=new Rectangle(10,4,8,6);//(x,y,ancho,alto)
-			//System.out.println(Objetos.centro(c));
+			Rectangle c=new Rectangle(10,4,8,6);//(x,y,ancho,alto)
+			System.out.println(Objetos.centro(c));
 		//Ejercicio 4
-			//Rectangle c=new Rectangle(10,4,8,6);//18;-4
-			//Point p1=new Point(10,4);
-			//System.out.println(Objetos.estaDentro(p1,c));
+			Rectangle c1=new Rectangle(10,4,8,6);//18;-4
+			Point p3=new Point(10,4);
+			System.out.println(Objetos.estaDentro(p3,c1));
 		//Ejercicio 5
-			//Point p1=new Point(10,5);
-			//Point p2=new Point(20,15);
-			//System.out.println(Objetos.puntoMedio(p1, p2));
+			Point p4=new Point(10,5);
+			Point p5=new Point(20,15);
+			System.out.println(Objetos.puntoMedio(p4, p5));
 		//Ejercicio 6
-		
+			System.out.println(Objetos.encuadrar(c, c1));
 		//Ejercicio 7
-			//Rectangle r1=new Rectangle(5, 5, 5, 5);//mas chico
-			//Rectangle r2=new Rectangle(0, 0, 10, 10);//El mas grande
-			//System.out.println(Objetos.estaContenido(r1, r2));
+			Rectangle r1=new Rectangle(5, 5, 5, 5);//mas chico
+			Rectangle r2=new Rectangle(0, 0, 10, 10);//El mas grande
+			System.out.println(Objetos.estaContenido(r1, r2));
 		//Ejercicio 8
-			
+			Rectangle r3=new Rectangle(5, 5, 5, 5);
+			Rectangle r4=new Rectangle(0, 0, 10, 10);
+			System.out.println(Objetos.interseccion(r3, r4));
 			
 		//Clase Fecha
-			//Fecha f1=new Fecha(7,4,2025);
-			//f1.imprimir();//No hace falta llamar a la otra proyecto java
-			//System.out.println(f1);
-		
+			Fecha f1=new Fecha(1,1,2025);
+			f1.imprimir();//No hace falta llamar al otro proyecto java
+			System.out.println(f1);
+			
 		//Fecha
 		//Ejercicio 1
-			//System.out.println(Fecha.diaDelMes(2, 2025));
-		
-		
+			System.out.println(Fecha.diaDelMes(2, 2025));
+		//Ejercicio 2
+			System.out.println(f1.esValida());
+		//Ejercicio 3
+			f1.avanzarDia();
+			f1.imprimir();
+		//Ejercicio 4
+			Fecha f2=new Fecha(1,1,2027);
+			System.out.println(f1.antesQue(f2));
+		//Ejecicio 5
+			System.out.println(f2.diaDelAnio());
+		//Ejercicio 6
+			System.out.println(f1.diasDeDiferenciaCon(f2));
+
 		
 		//Pixel
 		Pixel p = new Pixel(3,4,5);
@@ -53,8 +66,8 @@ public class Main {
 		
 		//Cuenta
 		double[]cuenta= {100,-20,120,-50};
-		Cuenta c = new Cuenta(cuenta,150,false);
-		System.out.println(c.esValida());
+		Cuenta c2 = new Cuenta(cuenta,150,false);
+		System.out.println(c2.esValida());
 		
 	}
 
